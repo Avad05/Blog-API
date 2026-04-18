7974f4b3cbbc1d523fc3553c8b69e5498d436c61
Avadhut <avadhutkaskar@gmail.com>
2026-04-18 14:35:43 +0530

    chore: update API_BASE_URL to use production endpoint in client configurations

--- client-public/src/config.js
+++ client-public/src/config.js
@@ -1,3 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL ||
+  (import.meta.env.PROD
+    ? "https://blog-api-31hl.onrender.com/api"
+    : "http://localhost:5000/api");
 
 export default API_BASE_URL;
